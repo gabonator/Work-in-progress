7958a5d965245886b31b7d821a064855b1fbe633
Milos Svasek <svasek@gmail.com>
2016-06-09 00:12:45 +0200

    Fixing file format
    Fixing file format: bash scripts must be in unix file format (LF).

--- SynologySearchEngine/Plugin/package/build.bat
+++ SynologySearchEngine/Plugin/package/build.bat
@@ -1,5 +1,5 @@
 cd ..\service
 tar zcf ..\package\package.tgz *
-cd ..\package 
-tar zcf ..\ulozto.spk INFO PACKAGE_ICON_256.PNG PACKAGE_ICON.PNG package.tgz
+cd ..\package
+tar zcf ..\ulozto.spk INFO PACKAGE_ICON_256.PNG PACKAGE_ICON.PNG package.tgz scripts
 del ..\package\package.tgz
--- SynologySearchEngine/Plugin/package/build.sh
+++ SynologySearchEngine/Plugin/package/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ../service
 tar zcf ../package/package.tgz *
-cd ../package 
-tar zcf ../ulozto.spk INFO PACKAGE_ICON_256.PNG PACKAGE_ICON.PNG package.tgz
+cd ../package
+tar zcf ../ulozto.spk INFO PACKAGE_ICON_256.PNG PACKAGE_ICON.PNG package.tgz scripts
 rm -f ../package/package.tgz
